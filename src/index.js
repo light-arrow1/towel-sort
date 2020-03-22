d3f5f68e0b1dc7a6b351fc59d2ed1e7de37f8d2c
Arrow <light.shaft.1@gmail.com>
2020-03-22 20:11:50 +0300

    init: add code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports =  function towelSort (matrix) {
+  let arrSort = [];
+  if (matrix){
+      matrix.forEach(function (item, i) {
+          if (i % 2 !== 0) {
+              arrSort.push(...item.reverse());
+          } else {   arrSort.push(...item);}
+      });
+  }
+  return arrSort;
+  
 }
